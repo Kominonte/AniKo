1262b9682d22453eadfb332d3970013a0eb3aea8
Kominonte <121425131+Kominonte@users.noreply.github.com>
2024-08-19 10:03:43 +0300

    Add files via upload

---
+++ app/services/router.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\services;
+
+class Router
+{
+	private static $list = [];
+
+	public static function page($uri, $page_name) {
+		self::$list[] = [
+			"uri" => $uri,
+			"page" => $page_name
+		];
+	}
+
+	public static function post($uri, $class, $method, $formdata = false, $files = false){
+		self::$list[] = [
+			"uri" => $uri,
+			"class" => $class,
+			"method" => $method,
+			"post" => true,
+			"formdata" => $formdata,
+			"files" => $files
+		];
+	}
+
+	public static function enable(){
+		 $query = ($_GET['q']);
+
+		foreach (self::$list as $route) {
+
+			if($route["uri"] === '/' . $query){
+				  if ($route["post"] === true && $_SERVER["REQUEST_METHOD"] === "POST"){
+					$action = new $route["class"];
+					$method = $route["method"];
+					
+					if($route["formdata"] && $route["files"]){
+						$action->$method($_POST, $_FILES);
+					}elseif($route["formdata"] && !$route["files"]){
+						$action->$method($_POST);
+					}else{
+						$action->$method($_POST);
+					}
+					die();
+				}else{
+					if($route["uri"] === '/' . $query){
+						require_once "views/pages/" . $route['page'] . ".php";
+						die();
+					}	
+				}	
+			}	
+		}
+
+		self::not_found_page();
+
+	}
+
+	private static function not_found_page(){
+		require_once "views/errors/not_found.php";
+	}
+
+	public static function redirect($page){
+		require_once "views/pages/" . $page . ".php";
+		die();
+	}
+
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+use	App\services\App;
+
+	require_once __DIR__ . "/vendor/autoload.php";	//Подключаем autoload
+	App::start();
+	require_once __DIR__ . "/router/routers.php";   //Подключаем список роутов
+	
+	// \App\services\Router::test();
+	// var_dump($_GET['q']);
