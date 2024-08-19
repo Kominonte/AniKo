00d1f262b983de1b5b09ac930ddc09f4dcef70a2
Kominonte <121425131+Kominonte@users.noreply.github.com>
2024-08-19 10:02:18 +0300

    Add files via upload

---
+++ AniKo/views/errors/not_found.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/x-icon" href="../assets/icon/aniko-nofon.png">
+	<link rel="stylesheet" href="../assets/css/not_found.css">
+	<title>Страница не найдена</title>
+</head>
+<body>
+
+	<img id="background" src="../assets/background/error.jpg">
+	
+	<div class="error-message-box">
+		<h1 class="error-label">Страница не найдена :(</h1>
+		<a href="/" class="go-home-btn">Домой ?</a>
+	<div>
+	
+</body>
+</html>
